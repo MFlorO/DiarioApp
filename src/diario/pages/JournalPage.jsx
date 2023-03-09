5bd0ac95ff1e330cb8fff30b916e64564ee82721
Flori O <floroldani@hotmail.com>
2023-03-09 16:35:14 -0300

    addBoton

--- src/diario/pages/JournalPage.jsx
+++ src/diario/pages/JournalPage.jsx
@@ -1,10 +1,29 @@
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout";
 import { NoteView, NothingSelectedView } from "../views";
+import AddIcon from '@mui/icons-material/Add';
+
 const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddIcon sx={{ fontSize: 30 }} />
+      </IconButton>
+
     </JournalLayout>
   );
 };
--- src/diario/views/NothingSelectedView.jsx
+++ src/diario/views/NothingSelectedView.jsx
@@ -7,7 +7,7 @@ const NothingSelectedView = () => {
     <Stack
       alignItems="center"
       justifyContent="center"
-      sx={{ minHeight: '89vh', width:'81vw' , backgroundColor: 'primary.main', borderRadius: 3 }}
+      sx={{ minHeight: '89vh', maxWidth:'81vw',  minWidth:'79vw', backgroundColor: 'primary.main', borderRadius: 3 }}
     >
         <Grid>
             <StarIcon sx={{ fontSize: 100, color: 'white' }} />
