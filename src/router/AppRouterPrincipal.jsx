d3b3573d12d8ff66d32104ed4ea320670b965d3e
Flori O <floroldani@hotmail.com>
2023-03-15 20:55:31 -0300

    custom hook para controlar la autenticacion

--- src/router/AppRouterPrincipal.jsx
+++ src/router/AppRouterPrincipal.jsx
@@ -1,42 +1,22 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "~/auth/router";
 import { DiarioRoutes } from "~/diario/router";
 import { Loading } from "~/ui";
-import { FirebaseAuth } from "~/firebase/config";
-import { onAuthStateChanged } from "firebase/auth";
-import { logout, login } from "~/redux";
-
+import { useCheckAuth } from "~/hooks";
 
 
 
 const AppRouterPrincipal = () => {
 
-  const dispatch = useDispatch()
-
-
-  const { status } = useSelector( state => state.auth)
-
-  useEffect(() => {
-
-    onAuthStateChanged(FirebaseAuth, async(user) => {
-      if( !user ) return dispatch(logout());
-
-      const {uid, email, photoURL, displayName} = user
-
-      dispatch(login({uid, email, photoURL, displayName}))
-    })
-
-  }, [])
-  
+  const status = useCheckAuth()
 
   if ( status === 'checking' ) return <Loading />
   
   return (
     <Routes>
-
-         {    
+      
+        {    
           (status === 'authenticated')
            ? <Route path="/*" element={ <DiarioRoutes /> } />
            : <Route path="/auth/*" element={ <AuthRoutes /> } />
